a7fe181719d9f97325bea0a9d3475fc9e91f0a1d
Pascual <48403430+PBoleander@users.noreply.github.com>
2020-07-28 16:10:43 +0200

    Añadido el centrar en pantalla

--- general/Pong.java
+++ general/Pong.java
@@ -1,6 +1,8 @@
 package general;
 
 import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 class Pong extends JFrame {
 
@@ -15,7 +17,21 @@ class Pong extends JFrame {
 
         pack();
 
+        centrarEnPantalla();
+
         setVisible(true);
+    }
+
+    private void centrarEnPantalla() {
+        // Averigua el tamaño de la pantalla
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int anchuraPantalla = (int) screenSize.getWidth();
+        int alturaPantalla = (int) screenSize.getHeight();
+
+        // Centra la ventana sabiendo su tamaño
+        int anchuraVentana = this.getWidth();
+        int alturaVentana = this.getHeight();
+        this.setLocation((anchuraPantalla - anchuraVentana) / 2, (alturaPantalla - alturaVentana) / 2);
     }
 
     public static void main(String[] args) {
